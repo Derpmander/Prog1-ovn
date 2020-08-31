c2e2d3f25fc65a5c2d69976abc48b949fb5f4b2c
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-08-31 14:14:12 +0200

    Övning 1.4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Prog1-ovn
  övningar för prog 1
+
+Detta är ett övningrepo för kursen progammering 1.
---
+++ uppgift 1.4.py
@@ -0,0 +1,5 @@
+namn=input("vad heter du? ")
+print("hej",namn)
+
+namn2=input("Efternamn då? ")
+print(namn,namn2)
