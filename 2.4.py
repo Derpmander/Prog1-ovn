7ca7234e2565275586730f5f9782f425de7a4e26
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-09-07 15:01:04 +0200

    2.4 och okomplett 2.5 i programmering 1

---
+++ 2.4.py
@@ -0,0 +1,8 @@
+
+svar=input( "Hur lång är kvadrates sida? ")
+n=float(svar)
+k=n*n
+h=n+n+n+n
+
+print(f"Kvadratens area är {k:2.2f}")
+print(f"Kavdratens omkrets är {h:2.2f}")
---
+++ 2.5.py
@@ -0,0 +1,4 @@
+svar=input("vad är varans pris med moms? ")
+x=int(svar)
+m=0.25
+p=x*m
