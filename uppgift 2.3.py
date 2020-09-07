caf79702c625d76c8d932767f2a4f235d015d545
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-09-07 14:36:32 +0200

    Uppgift 2.3 programmering + extra

---
+++ uppgift 2.3.py
@@ -0,0 +1,13 @@
+svar=input("skriv ett tal: ")
+x=float(svar)
+y=x*x
+print(f"talet i kvadrat är {y:2.2f}")
+
+i=19
+print(f"Resultat:{i}")
+
+j=107
+print(f"Resultat:{j:5}")
+
+k=4
+print(f"Resultat:{k:03}")
