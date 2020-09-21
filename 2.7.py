74e092b56226cacb971360033fa554737f91b049
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-09-21 14:47:46 +0200

    uppgift 2.7

--- 2.7.py
+++ 2.7.py
@@ -1,3 +1,9 @@
 import math
-pi=3,14
-print=pi
+rad=input("Vad är cirkelns radie: ")
+x=float(rad)
+y=3.141
+area=x*x*y
+print("arean är",area)
+z=x+x
+omkrets=z*y
+print(f"omkretsen är {omkrets:2.3f}") 
