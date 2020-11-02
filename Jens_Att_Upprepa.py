18eca96dd5c0970f5ad4d4e2ecc5864b81332a71
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-11-02 14:11:33 +0100

    Create Jens_Att_Upprepa.py

---
+++ Jens_Att_Upprepa.py
@@ -0,0 +1,14 @@
+import random
+gissa=0
+försök=0
+correct= random.randint(1,3)
+while gissa !=correct:
+    försök+=1
+    gissa=int(input("Skriv din gissning, Helst ett hel tal: "))
+    if gissa <correct:
+        print("för litet")
+    elif gissa >correct:
+        print("för stort")
+else:
+    print("Rätt")
+print ("Antal försök:",försök)
