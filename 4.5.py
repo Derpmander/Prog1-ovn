1e541cd9a97eb4f53c55b24b5c3655ece2250da7
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-11-09 14:41:21 +0100

    Uppgifter från 4.5 till 4.8 varav 4.5 har komentarer

--- 4.5.py
+++ 4.5.py
@@ -1,19 +1,13 @@
 while True:
-    n=int(input("n?, Skriv ett tal <=0 för att sluta: "))
-    if n <= 0:
-        break
-    summa = 0
-    k=1
-    while k<=n:
-        summa=summa+k
-        k=k+1
-    print("Summan blir", summa)
-
-    startHöjd=int(input("Start höjd:"))
-höjd=startHöjd
-förlust=0.7
-while höjd>0.01:
-    höjd=höjd*förlust
-    studsar=startHöjd/höjd
-    studsar=int(studsar)
-print(f"Bollen studsar {studsar} gånger")
+    startHöjd=int(input("Start höjd:"))#Anger vad start höjden är 
+    if startHöjd <= 0:#Händer om det blir ett minus tal
+        print("Good job you failed")#Skriver ut en text om det blir ett minus tal
+        break #stannar koden
+    elif startHöjd >0:#Sker om det blir ett positivt tal
+        höjd=startHöjd#Anger höjd som startHöjden
+        förlust=0.7#Anger vad som förloras vid varje studs
+        while höjd>0.01: #Anger när den ska sluta studsa
+            höjd=höjd*förlust #Beräknar höjden
+            studsar=startHöjd/höjd #Beräknar antal studsar
+            studsar=int(studsar) #Anger studsar
+        print(f"Bollen studsar {studsar} gånger") #Skriver ut hur många gånger bollen studsat
